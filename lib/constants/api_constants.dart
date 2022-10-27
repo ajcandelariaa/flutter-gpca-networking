3f881e0e178afc114a1d67e7ce9f771aae6f7805
Aj Candelaria <ajajcandelaria@gmail.com>
2022-10-27 21:36:22 +0400

    fetching data modified

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,5 +1,4 @@
 class ApiConstants {
-  
   static const String baseUrl = '';
   static const String eventsListEndPoint = '';
 }
--- lib/screens/events_list_screen.dart
+++ lib/screens/events_list_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:gpca_networking/constants/api_constants.dart';
-import 'package:gpca_networking/models/event_model.dart';
 import 'package:gpca_networking/providers/event_provider.dart';
-import 'package:gpca_networking/services/api_service.dart';
 import 'package:gpca_networking/widgets/events/event.dart';
 import 'package:provider/provider.dart';
 
 class EventsListScreen extends StatelessWidget {
   const EventsListScreen({Key? key}) : super(key: key);
   static const routeName = '/events-list-screen';
+
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<EventProvider>(context, listen: false).getEvents();
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -17,33 +19,16 @@ class EventsListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('GPCA Networking'),
       ),
-      body: FutureBuilder(
-        future: Provider.of<EventProvider>(context, listen: false).getEvents(),
-        builder: (ctx, snapshot) =>
-            snapshot.connectionState == ConnectionState.waiting
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : Consumer<EventProvider>(
-                    child: const Center(
-                      child: Text('Got no events yet, start adding some!'),
-                    ),
-                    builder: (ctx, result, ch) => result.events.isEmpty
-                        ? ch!
-                        : ListView.builder(
-                            itemCount: result.events.length,
-                            itemBuilder: (ctx, index) {
-                              return Event(
-                                eventId: result.events[index].eventId,
-                                eventName: result.events[index].eventName,
-                                eventDate: result.events[index].eventDate,
-                                eventVenue: result.events[index].eventVenue,
-                                eventLogo: result.events[index].eventLogo,
-                              );
-                            },
-                          ),
-                  ),
-      ),
+      // body: ListView.builder(itemBuilder: (ctx, index) {
+      //   return Event(
+      //     eventId: result.events[index].eventId,
+      //     eventName: result.events[index].eventName,
+      //     eventDate: result.events[index].eventDate,
+      //     eventVenue: result.events[index].eventVenue,
+      //     eventLogo: result.events[index].eventLogo,
+      //   );
+      // }),
+      
     );
   }
 }
