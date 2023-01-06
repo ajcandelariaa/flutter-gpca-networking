331caa177117fee693d31fb4eb7d7e4d434f2040
Aj Candelaria <ajajcandelaria@gmail.com>
2023-01-06 15:14:03 +0400

    Update feedback_form.dart

--- lib/widgets/forms/feedback_form.dart
+++ lib/widgets/forms/feedback_form.dart
@@ -6,7 +6,33 @@ class FeedbackForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Form"),
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Please rate your experience:"),
+            SizedBox(
+              height: 15,
+            ),
+            Text("Additional Comments:"),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Submit"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
