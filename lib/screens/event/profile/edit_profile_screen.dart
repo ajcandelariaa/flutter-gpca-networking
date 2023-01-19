41b3671657072a575eb54d95a76b08bb190a7e9e
Aj Candelaria <ajajcandelaria@gmail.com>
2023-01-19 15:54:13 +0400

    Edit profile form modified

--- lib/screens/event/profile/edit_profile_screen.dart
+++ lib/screens/event/profile/edit_profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gpca_networking/providers/user_profile_provider.dart';
 import 'package:gpca_networking/widgets/forms/edit_profile_form.dart';
+import 'package:provider/provider.dart';
 
 class EditProfileScreen extends StatelessWidget {
   const EditProfileScreen({Key? key}) : super(key: key);
@@ -7,21 +9,23 @@ class EditProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProfileProvider>(context).getProfile(id: "1", eventId: "1");
     return Scaffold(
       appBar: AppBar(
-        title: Text("Edit profile"),
+        title: const Text("Edit profile"),
         centerTitle: true,
       ),
       body: Container(
-        margin: EdgeInsets.symmetric(
+        margin: const EdgeInsets.symmetric(
           horizontal: 20,
           vertical: 15,
         ),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              
-              EditProfileForm(),
+              EditProfileForm(
+                userData: user,
+              ),
             ],
           ),
         ),
--- lib/widgets/forms/edit_profile_form.dart
+++ lib/widgets/forms/edit_profile_form.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:gpca_networking/models/user_profile_model.dart';
 
 class EditProfileForm extends StatelessWidget {
-  const EditProfileForm({Key? key}) : super(key: key);
+  // final String id;
+  // final String eventId;
+  // final String username;
+  // final String salutation, firstName, middleName, lastName;
+  // final String emailAddress, mobileNumber, landlineNumber;
+  // final String companyName, jobTitle, country;
+  final UserProfileModel userData;
+
+  EditProfileForm({
+    required this.userData,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -9,11 +20,15 @@ class EditProfileForm extends StatelessWidget {
       child: Form(
         child: Column(
           children: [
-            TextFormField(),
+            TextFormField(
+              decoration: const InputDecoration(labelText: "First name"),
+              initialValue: userData.firstName,
+            ),
             TextFormField(),
             TextFormField(),
             TextFormField(
-              
+              decoration: const InputDecoration(labelText: "Last Name"),
+              initialValue: userData.lastName,
             ),
           ],
         ),
